1dfc68b750969fac884e340055d87de6542d48d8
Alexandra <undead@neofusion.ru>
2017-01-12 17:52:19 +0300

    Fix retrofit interface urls

--- app/src/main/java/com/example/undead/habrahabrrss/network/HabrApiInterface.java
+++ app/src/main/java/com/example/undead/habrahabrrss/network/HabrApiInterface.java
@@ -3,19 +3,24 @@ package com.example.undead.habrahabrrss.network;
 import com.example.undead.habrahabrrss.model.RssFeedResponse;
 
 import io.reactivex.Observable;
-import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface HabrApiInterface {
     @GET("interesting")
+    Observable<RssFeedResponse> getInteresting();
+
+    @GET("all")
+    Observable<RssFeedResponse> getAll();
+
+    @GET("best")
     Observable<RssFeedResponse> getTopDay();
 
-    @GET("interesting/weekly")
+    @GET("best/weekly")
     Observable<RssFeedResponse> getTopWeek();
 
-    @GET("interesting/monthly")
+    @GET("best/monthly")
     Observable<RssFeedResponse> getTopMonth();
 
-    @GET("interesting/all")
+    @GET("best/alltime")
     Observable<RssFeedResponse> getTopAll();
 }
